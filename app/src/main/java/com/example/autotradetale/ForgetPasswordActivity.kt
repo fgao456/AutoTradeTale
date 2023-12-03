f0aade01811cd26f7106e40d2e8ca9ee5bca7bdc
Gurjeet Singh <71973659+Gurjeets934@users.noreply.github.com>
2023-12-03 01:42:58 -0500

    Add files via upload

--- app/src/main/java/com/example/autotradetale/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/autotradetale/ForgetPasswordActivity.kt
@@ -1,8 +1,11 @@
 package com.example.autotradetale
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,6 +18,15 @@ import androidx.appcompat.app.AppCompatActivity
             setContentView(R.layout.activity_forgetpassword)
 
             val backTextView = findViewById<TextView>(R.id.backTextView)
+
+            val email = findViewById<EditText>(R.id.inputEmail)
+
+            val sp = applicationContext.getSharedPreferences("UserPreferences",
+                Context.MODE_PRIVATE)
+            val emailAddress = sp.getString("password","")
+
+            email.setText(emailAddress)
+
             backTextView.setOnClickListener{
                 // Redirect to the Sign Up Page after the button is clicked
                 val intent = Intent(this, MainActivity::class.java)
